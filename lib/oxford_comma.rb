41fa3d3eeaa31378a2d8bd2117686274c30e6efe
Ashley Mattam <amattam427@gmail.com>
2022-02-06 20:42:34 -0500

    commit tree 774381e3480b229a1a7fa300a2aee4578b77c41c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
+    return array.join(" and ") if array.length < 3
 
+    # insert 'and ' in front of the last string in the array
+    array[-1] = "and #{array[-1]}"
+  
+    array.join(", ")  
 end
